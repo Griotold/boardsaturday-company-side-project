c7377f655da2b3343bfcc9585fdab96ac991ac6a
Griotold <gotjd9773@naver.com>
2024-01-31 20:39:36 +0900

    Initial commit

---
+++ src/main/java/com/bizplus/boardsaturday/BoardsaturdayApplication.java
@@ -0,0 +1,13 @@
+package com.bizplus.boardsaturday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardsaturdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardsaturdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bizplus/boardsaturday/BoardsaturdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizplus.boardsaturday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardsaturdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
