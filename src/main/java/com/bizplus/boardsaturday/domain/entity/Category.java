5701a782dea345ea2b4ad2b9babf02fc72e1ff2f
bizplus033 <bizplus033@naver.com>
2024-02-02 15:36:38 +0900

    [category/entity] BaseEntity, BaseTimeEntity, Category 엔티티 추가

--- src/main/java/com/bizplus/boardsaturday/domain/entity/Category.java
+++ src/main/java/com/bizplus/boardsaturday/domain/entity/Category.java
@@ -1,11 +1,21 @@
 package com.bizplus.boardsaturday.domain.entity;
 
+import com.bizplus.boardsaturday.domain.common.BaseTimeEntity;
 import com.bizplus.boardsaturday.domain.type.CategoryStatus;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 
 @Entity
-public class Category {
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,4 +29,11 @@ public class Category {
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private CategoryStatus status;
+
+    public Category(String name, String description, int displayOrder, CategoryStatus status) {
+        this.name = name;
+        this.description = description;
+        this.displayOrder = displayOrder;
+        this.status = status;
+    }
 }
