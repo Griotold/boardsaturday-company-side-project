59fbd26ab04ff6c2003e62514f618637f2653ec5
bizplus033 <bizplus033@naver.com>
2024-02-07 15:01:31 +0900

    [entity] Post, Tag

---
+++ src/main/java/com/bizplus/boardsaturday/domain/entity/Post.java
@@ -0,0 +1,33 @@
+package com.bizplus.boardsaturday.domain.entity;
+
+import com.bizplus.boardsaturday.domain.common.BaseTimeEntity;
+import com.bizplus.boardsaturday.domain.type.DeleteStatus;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String title;
+
+    private String body;
+
+    @Enumerated(EnumType.STRING)
+    private DeleteStatus deleteStatus;
+
+
+}
---
+++ src/main/java/com/bizplus/boardsaturday/domain/entity/Tag.java
@@ -0,0 +1,34 @@
+package com.bizplus.boardsaturday.domain.entity;
+
+import com.bizplus.boardsaturday.domain.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public Tag(String name, Post post) {
+        this.name = name;
+        this.post = post;
+    }
+
+}
