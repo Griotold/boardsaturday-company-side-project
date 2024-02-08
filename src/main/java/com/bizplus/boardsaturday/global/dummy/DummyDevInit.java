7cbe0e8f782ed8dbc980af776c77e68a02d752e4
Griotold <gotjd9773@naver.com>
2024-02-08 16:49:21 +0900

    [운영환경, 테스트환경 분리]

--- src/main/java/com/bizplus/boardsaturday/global/dummy/DummyDevInit.java
+++ src/main/java/com/bizplus/boardsaturday/global/dummy/DummyDevInit.java
@@ -12,14 +12,4 @@ import org.springframework.transaction.annotation.Transactional;
 public class DummyDevInit {
 
     // 테스트 환경이랑 비즈니스 환경을 분리해라
-    @Transactional
-    @Bean
-    CommandLineRunner init(CategoryRepository categoryRepository) {
-        return (args -> {
-            for (int i = 0; i < 10; i++) {
-                Category category = new Category("카테고리" + i, "설명" + i, i, ActiveStatus.ACTIVE);
-                categoryRepository.create(category);
-            }
-        });
-    }
 }
