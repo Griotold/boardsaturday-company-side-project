7862838a98405c77686ca1f9a3c5dff508c36626
bizplus033 <bizplus033@naver.com>
2024-02-14 11:57:02 +0900

    [category/list] - CategoryResponse는 원상복귀, 새로운 dto 객체 만들었음

--- src/main/java/com/bizplus/boardsaturday/application/response/category/CategoryWithPostCountResponse.java
+++ src/main/java/com/bizplus/boardsaturday/application/response/category/CategoryWithPostCountResponse.java
@@ -4,22 +4,24 @@ import com.bizplus.boardsaturday.domain.entity.Category;
 import lombok.Getter;
 
 @Getter
-public class CategoryResponse {
+public class CategoryWithPostCountResponse {
     private final Long id;
     private final String name;
     private final String description;
     private final int displayOrder;
     private final String status;
+    private final Integer postCount;
 
-    public CategoryResponse(Category category) {
+    public CategoryWithPostCountResponse(Category category, Integer postCount) {
         this.id = category.getId();
         this.name = category.getName();
         this.description = category.getDescription();
         this.displayOrder = category.getDisplayOrder();
         this.status = category.getStatus().name();
+        this.postCount = postCount;
     }
 
-    public static CategoryResponse of(Category category) {
-        return new CategoryResponse(category);
+    public static CategoryWithPostCountResponse of(Category category, Integer postCount) {
+        return new CategoryWithPostCountResponse(category, postCount);
     }
 }
