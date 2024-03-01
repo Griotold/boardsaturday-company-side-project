1c9754df940ed394c6a63aaf7e4d7497a3089b75
Griotold <gotjd9773@naver.com>
2024-03-01 10:08:18 +0900

    [comment-entity] Comment 엔티티 뼈대

---
+++ src/main/java/com/bizplus/boardsaturday/domain/entity/Comment.java
@@ -0,0 +1,34 @@
+package com.bizplus.boardsaturday.domain.entity;
+
+import com.bizplus.boardsaturday.domain.common.BaseTimeEntity;
+import com.bizplus.boardsaturday.domain.type.ActiveStatus;
+import com.bizplus.boardsaturday.domain.type.DeleteStatus;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private ActiveStatus activeStatus;
+
+    @Enumerated(EnumType.STRING)
+    private DeleteStatus deleteStatus;
+
+}
