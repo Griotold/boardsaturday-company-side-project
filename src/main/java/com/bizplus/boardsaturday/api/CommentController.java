9dea97b37ddbd8a761cf2f2ca1f0f0c13fd3f090
Griotold <gotjd9773@naver.com>
2024-03-01 12:31:55 +0900

    [comment/create] Controller, Serivce, HTTP 요청 파일

---
+++ src/main/java/com/bizplus/boardsaturday/api/CommentController.java
@@ -0,0 +1,31 @@
+package com.bizplus.boardsaturday.api;
+
+import com.bizplus.boardsaturday.application.request.comment.CreateCommentRequest;
+import com.bizplus.boardsaturday.application.service.CommentService;
+import com.bizplus.boardsaturday.global.response.ResponseDto;
+import com.bizplus.boardsaturday.global.response.ResponseStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comments")
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody @Validated CreateCommentRequest request,
+                                    BindingResult bindingResult) {
+        commentService.create(request);
+        ResponseDto<Object> responseDto
+                = new ResponseDto<>(ResponseStatus.GOOD.getCode(), ResponseStatus.GOOD.getMessage(), null);
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/bizplus/boardsaturday/application/service/CommentService.java
+++ src/main/java/com/bizplus/boardsaturday/application/service/CommentService.java
@@ -1,10 +1,16 @@
 package com.bizplus.boardsaturday.application.service;
 
+import com.bizplus.boardsaturday.application.component.comment.CommentCreator;
+import com.bizplus.boardsaturday.application.request.comment.CreateCommentRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+    private final CommentCreator commentCreator;
 
+    public void create(CreateCommentRequest request) {
+        commentCreator.create(request);
+    }
 }
