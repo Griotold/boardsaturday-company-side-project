7aaf971a7866c2e6304dd6db08b2fb1ddfc44f17
Griotold <gotjd9773@naver.com>
2024-03-01 18:08:33 +0900

    [comment/list] Controller, Service

--- src/main/java/com/bizplus/boardsaturday/api/CommentController.java
+++ src/main/java/com/bizplus/boardsaturday/api/CommentController.java
@@ -1,10 +1,15 @@
 package com.bizplus.boardsaturday.api;
 
 import com.bizplus.boardsaturday.application.request.comment.CreateCommentRequest;
+import com.bizplus.boardsaturday.application.response.comment.CommentResponse;
 import com.bizplus.boardsaturday.application.service.CommentService;
+import com.bizplus.boardsaturday.domain.type.ActiveStatus;
 import com.bizplus.boardsaturday.global.response.ResponseDto;
 import com.bizplus.boardsaturday.global.response.ResponseStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -16,6 +21,21 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+    @GetMapping
+    public ResponseEntity<?> comments(@RequestParam(value = "status", required = false) ActiveStatus activeStatus,
+                                      @RequestParam(value = "content", required = false) String content,
+                                      @PageableDefault(size = 5) Pageable pageable) {
+        Page<CommentResponse> all =
+                commentService.commentList(activeStatus, content, pageable);
+
+        ResponseDto<Page<CommentResponse>> responseDto
+                = new ResponseDto<>(ResponseStatus.GOOD.getCode(),
+                    ResponseStatus.GOOD.getMessage(),
+                    all);
+
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody @Validated CreateCommentRequest request,
